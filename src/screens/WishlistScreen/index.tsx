78a78ce56afba92392e5d6521a63b7949149a1fa
Sambid10 <sambidshk10@gmail.com>
2025-08-08 14:22:29 +0545

    displayed fav movies in favmovie screen

--- src/screens/WishlistScreen/index.tsx
+++ src/screens/WishlistScreen/index.tsx
@@ -1,52 +1,94 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View, StyleSheet } from 'react-native';
+import {
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
 import axiosInstance from '../../axios/axios';
 import { Movie } from '../../types/MovieTypes';
 import FastImage from 'react-native-fast-image';
 import { getAuth } from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/native';
+import { NavigationProp } from '../../naviagation/types';
 
 export default function WishListScreen() {
+  const navigtion=useNavigation<NavigationProp>()
   const [datas, setDatas] = useState<Movie[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [loading, setLoading] = useState(false);
   const auth = getAuth();
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const res = await axiosInstance.get(
+        `/account/22105215/favorite/movies?language=en-US&page=1&sort_by=created_at.asc`,
+      );
+      setDatas(res.data.results);
+    } catch (err) {
+      setLoading(false);
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
   useEffect(() => {
-    const fetchdata = async () => {
-      try {
-        const res = await axiosInstance.get(
-          `/account/22105215/favorite/movies?language=en-US&page=1&sort_by=created_at.asc`,
-        );
-        setDatas(res.data.results);
-      } catch (error) {
-        console.error('Failed to fetch favorites:', error);
-      }
-    };
-    fetchdata();
+    fetchData();
   }, [auth.currentUser?.uid]);
 
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchData();
+    setRefreshing(false);
+  };
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 21, color: 'white', fontWeight: 'bold' }}>
+      <Text
+        style={{
+          fontSize: 21,
+          color: 'white',
+          fontWeight: 'bold',
+          marginBottom: 16,
+        }}
+      >
         Your Favourites
       </Text>
-      <FlatList
-        data={datas}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({ item }) => (
-          <FastImage
-            style={styles.image}
-            resizeMode="cover"
-            source={{
-              uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
-            }}
-          />
-        )}
-        ListEmptyComponent={
-          <Text style={styles.emptyText}>No Favourites..</Text>
-        }
-        contentContainerStyle={
-          datas.length === 0 && { flex: 1, justifyContent: 'center' }
-        }
-      />
+      {loading ? (
+        <ActivityIndicator color={'white'} />
+      ) : (
+        <FlatList
+          data={datas}
+          keyExtractor={item => item.id.toString()}
+          numColumns={2}
+          columnWrapperStyle={{display:"flex",justifyContent:"flex-start"}}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+            onPress={()=>navigtion.navigate("MovieDetails",{movieId:item.id})}
+              style={{
+                width: '45%',
+                margin: 8,
+              }}
+            >
+              <FastImage
+                style={{ width: '100%', height: 200, borderRadius: 12,borderColor:"#4B5563",borderWidth:StyleSheet.hairlineWidth }}
+                resizeMode="cover"
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                }}
+              />
+            </TouchableOpacity>
+          )}
+          ListEmptyComponent={
+            <Text style={styles.emptyText}>No Favourites..</Text>
+          }
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+        />
+      )}
     </View>
   );
 }
@@ -56,8 +98,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 14,
     backgroundColor: '#002335',
-    display: 'flex',
-    gap: 24,
   },
   image: {
     width: 140,
